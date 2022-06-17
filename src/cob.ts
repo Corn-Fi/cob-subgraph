221942f8157b951fa456a8aef0a88863d69b158f
Korey Corkscrew <cameronbiniamow@Camerons-MacBook-Pro.local>
2022-06-17 17:47:04 -0500

    init

--- src/cob.ts
+++ src/cob.ts
@@ -1,68 +1,49 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal, BigInt, dataSource, log } from "@graphprotocol/graph-ts"
 import {
-  cob,
+  cob as CobContract,
   Approval,
   OwnershipTransferred,
-  Transfer
-} from "../generated/cob/cob"
-import { ExampleEntity } from "../generated/schema"
+  Transfer,
+  cob
+} from "../generated/undefined/cob"
+import { Cob } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+const COB_ADDRESS = Address.fromString("0x648FA1E7Dd2722Ba93EC4Da99f2C32347522a37C")
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.INITIALSUPPLY(...)
-  // - contract.MAXSUPPLY(...)
-  // - contract.REWARDSSUPPLY(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
+export function fetchCobContract(): CobContract {
+  return CobContract.bind(COB_ADDRESS)
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function fetchCob(): Cob {
+  let cob = Cob.load(COB_ADDRESS.toHexString())
+  if(cob === null) {
+    const cc = fetchCobContract()
+    cob = new Cob(COB_ADDRESS.toHexString())
+    cob.name = cc.name()
+    cob.symbol = cc.symbol()
+    cob.decimals = BigInt.fromI32(cc.decimals())
+    cob.totalSupply = BigDecimal.zero()
+    cob.save()
+  }
+  return cob as Cob
+}
 
-export function handleTransfer(event: Transfer): void {}
+export function handleTransfer(event: Transfer): void {
+  let cob = fetchCob()
+  log.debug('from {} to {} value {}', [event.params.from.toHexString(), event.params.to.toHexString(), event.params.value.toString()])
+  const denominator = BigInt.fromI32(10).pow(18).toBigDecimal()
+  if(Address.zero().equals(event.params.from)) {
+    log.debug('from zero', [])
+    const cobMinted = event.params.value.toBigDecimal().div(denominator)
+    cob.totalSupply = cob.totalSupply.plus(cobMinted)
+  }
+  else if(Address.zero().equals(event.params.to)) {
+    log.debug('to zero', [])
+    let cob = fetchCob()
+    const cobBurned = event.params.value.toBigDecimal().div(denominator)
+    cob.totalSupply = cob.totalSupply.minus(cobBurned)
+  }
+  cob.save()
+}
